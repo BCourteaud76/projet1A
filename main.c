c0144ea420b68585fe7e296a6830977b4ea25e1e
BCourteaud76 <bastien.courteaud@gmail.com>
2019-05-02 09:26:20 +0200

    Commit 4 avec les données pour l'ajout de la structure

---
+++ main.c
@@ -0,0 +1,6 @@
+#include "struct.h"
+
+int main (){
+
+  
+}
---
+++ struct.h
@@ -0,0 +1,12 @@
+#include "struct.c"
+
+typedef struct { char* nom; double x,y ; L_ARC voisins;} T_SOMMET ;
+
+typedef struct { int arrivee; double cout; } T_ARC ;
+
+typedef struct lsucc { T_ARC val; struct lsucc* suiv ; }* L_ARC;
+
+typedef struct liste {
+  struct liste* suiv;
+  double distance;
+    }* LISTE;
