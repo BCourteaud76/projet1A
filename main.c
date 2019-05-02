db66d527fdc619883da2d092c90416010bc084b7
BCourteaud76 <bastien.courteaud@gmail.com>
2019-05-02 10:17:56 +0200

    commit 6 fin séance 1

--- main.c
+++ main.c
@@ -8,10 +8,28 @@ int main (){
 
 GRAPHE lectureFichier(char* fileName){
     FILE f = fopen(fileName,rt);
+    int nl,nbArc;
     fsanf(f,"%lf %lf", &nl, &nbArc);
+    T_SOMMET* graph=calloc(nl, sizeof(T_SOMMET));
     fgets(f);
-    int i;
+    int i,d;
+    double x,y;
+    char l[4],n[40];
     for(i=0; i<nl; i++){
-
+      fsanf(f,"%d %lf %lf %s %s", &d, &(graph[i].x), &(graph[i].y), l, n);
+      int j;
+      for (j=0; j<3;j++){
+        graph[i].ligne[j]=l[j];
+      }
+      for (j=0; j<40;j++){
+        graph[i].nom[j]=n[j];
+      }
+    }
+    fgets(f);
+    int origine,destination;
+    double longueur;
+    for(i=0; i<nbArc; i++){
+      fsanf(f,"%d %d %lf", &origine, &destination, &longueur);
+      ajout_queue(graph[origine].voisins, destination, longueur); //a definir proprement
     }
 }
--- struct.h
+++ struct.h
@@ -1,14 +1,19 @@
 
 
 typedef struct {
-  char*nom;
+  char* nom;
   char* ligne;
   double x,
   double y;
   L_ARC voisins;
 } GRAPHE;
 
-typedef struct { char* nom; double x,y ; L_ARC voisins;} T_SOMMET ;
+typedef struct {
+  char nom[40];
+  char ligne[4];
+  double x,y ;
+  L_ARC voisins;
+} T_SOMMET ;
 
 typedef struct { int arrivee; double cout; } T_ARC ;
 
